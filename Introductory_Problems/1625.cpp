a033cb39608a2f3b30b027ffbd338d866a26fceb
Sasha Sky <sashaskyru@gmail.com>
2022-07-30 01:07:12 +0500

    std::vector slow af... Noted

--- Introductory_Problems/1625.cpp
+++ Introductory_Problems/1625.cpp
@@ -2,24 +2,25 @@
 #include <map>
 #include <vector>
 
-bool grid[81] {};
-std::string path;
-std::map<char, int> moves {{'D',9},{'L',-1},{'U',-9},{'R',1}};
+bool grid[81];
+int step[48];
+std::map<char, int> moves {{'D',9},{'L',-1},{'U',-9},{'R',1},{'?',0}};
 
-int move(int pIndex, int pos) {
-	if(pIndex==48||pos==64)return pIndex==48?1:0;
+int move(int i, int pos) {
+	if(pos==64)return i==48?1:0;
 	if(grid[pos-9]==grid[pos+9]&&grid[pos-1]==grid[pos+1])return 0;
 	grid[pos]=1;
-	int r=0; std::vector<int> qmoves {9,-1,-9,1};
-	if (path[pIndex]!='?') {qmoves = {moves[path[pIndex]]};}
-	for (int step:qmoves){if(!grid[pos+step]) r+=move(pIndex+1,pos+step);}
+	int r=0; 
+	if (step[i]) {if(!grid[pos+step[i]]) r+=move(i+1,pos+step[i]);}
+	else {for (int step:{9,-1,-9,1}) {if(!grid[pos+step]) r+=move(i+1,pos+step);}}
 	grid[pos]=0;
 	return r;
 }
 
 int main() {
 	std::cin.tie(0);
-	std::cin>>path;
+	std::string path; std::cin>>path;
+	int it = 0;	for (char ch:path) {step[it++] = moves[ch];}
 	for (int i=0;i<81;i++)if(i<=9||i>=72||i%9==0||i%9==8)grid[i]=1;
 	std::cout<<move(0, 10)<<std::endl;
 	return 0;
