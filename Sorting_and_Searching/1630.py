7267216211e0a648aeb638dfb32516534c6a50e3
Sasha Sky <sashaskyru@gmail.com>
2022-08-05 09:10:48 +0500

    1630 finally. Map was too slow... ok...

--- Sorting_and_Searching/1630.py
+++ Sorting_and_Searching/1630.py
@@ -1,35 +1,10 @@
-def solve(tasks):
-	tasks.sort()
-	points, time = 0, 0
-	for t in tasks:
-		time += t[0]
-		points += t[1]-time
-	print(points)
-
-
-
-# source of cringe, don't watch below this line!
 import sys
 
-LOCAL = False
-
-
-try: 
-	sys.stdin=open('in.txt','r')
-	# sys.stdout=open('out.txt', 'w')
-	# sys.stdout=open('/dev/null', 'w')
-	LOCAL = True
-	def log(*a,**kwa): print(*a,**kwa,file=sys.stderr)
-	from time import time
-	start = time()
-except FileNotFoundError:	
-	pass
-
 I=input
-MIIS=lambda:[*map(int,I().split())]
-
 N = int(I())
-solve([MIIS() for _ in range(N)])
-
-if LOCAL:
-	log(f'Finished in {time()-start:.3f}sec')
+tasks = sorted([(int(d),int(f)) for d, f in (l.split() for l in sys.stdin.readlines())])
+points, time = 0, 0
+for t in tasks:
+	time += t[0]
+	points += t[1]-time
+print(points)
